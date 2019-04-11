b875807ff811086dde6dbbd9e2f1cd46bb0f8db0
Tadele Yednkachw <tadeleyednkachw@gmail.com>
2019-04-11 05:48:54 -0800

    database added

---
+++ word.py
@@ -0,0 +1,48 @@
+from flask import Flask, render_template, request, escape
+from funtions import search4letters
+app = Flask(__name__)
+@app.route('/search', methods=['POST'])
+def do_search() -> str:
+    phrase= request.form['phrase']
+    letters= request.form['letters'] 
+    title= 'Here are your results: '
+    results= str(search4letters(phrase, letters))
+    log_request(request, results)
+    return render_template('result.html',
+                            the_phrase= phrase,
+			    the_letters= letters,
+			    the_title=title,
+			    the_results= results,)
+@app.route('/')
+@app.route('/entry')
+def entry_page() -> 'html':
+    return render_template('entry.html',
+			    the_title='Welcome to this page')
+def log_request(req: 'flask_request', res: str) -> None:
+    from mysql.connector import connect
+    dbconfig = {'host':'127.0.0.1', 'user':'root', 'database':'wordlogdb',}
+    conn = connect(**dbconfig)
+    cursor = conn.cursor()
+    _SQL = '''insert into log (phrase, letters, ip, browser_string, results) values (%s,%s,%s,%s,%s) '''
+    cursor.execute(_SQL,(req.form['phrase'], req.form['letters'], req.remote_addr, req.user_agent.browser, res, ))
+    conn.commit()
+    cursor.close()
+    conn.close()
+    '''with open('wordsearch.log', 'a') as log:
+        print(req.form, req.remote_addr, req.user_agent, res, file= log, sep='|')'''
+
+@app.route('/viewlog')
+def view_the_log() -> 'html':
+    contents= []
+    with open('wordsearch.log') as log:
+        for line in log: 
+            contents.append([])
+            for item in line.split('|'): 
+                contents[-1].append(escape(item))
+    titles = ('Form Data', 'Remote_addr', 'User_agent', 'Results')
+    return render_template('viewlog.html',
+                            the_title='View Log',
+                            the_row_titles= titles,
+                            the_data= contents,)
+if __name__ == '__main__':
+    app.run(debug=True)
