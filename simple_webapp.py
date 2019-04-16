4710f0d8560b9c25e1d4c8937f50709d4eaa0ae0
Tadele Yednkachw <tadeleyednkachw@gmail.com>
2019-04-16 05:11:04 -0800

    decorator for simple webapp added

--- simple_webapp.py
+++ simple_webapp.py
@@ -1,4 +1,5 @@
 from flask import Flask, session
+from checker import check_logged_in
 app = Flask(__name__)
 
 @app.route('/')
@@ -6,12 +7,15 @@ def index()-> str:
     return 'this is the index page '
 
 @app.route('/page1')
+@check_logged_in
 def page1()-> str:
     return 'this is the page1 '
 @app.route('/page2')
+@check_logged_in
 def page2()-> str:
     return 'this is the page2 '
 @app.route('/page3')
+@check_logged_in
 def page3()-> str:
     return 'this is the page3 '
 
@@ -25,11 +29,6 @@ def do_logout()-> str:
     session.pop('logged_in')
     return 'You are now logged out'
 
-@app.route('/status')
-def check_status() -> str:
-    if 'logged_in' in session:
-        return 'You are currently logged in'
-    return 'You are Not logged in'
 app.secret_key = 'YouWillNeverGuessMysecretKey'
 if __name__ == '__main__':
     app.run(debug = True)
