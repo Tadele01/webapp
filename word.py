5c990ca73d8bcc000fdea4384a5bf8c8a03f9654
Tadele Yednkachw <tadeleyednkachw@gmail.com>
2019-04-17 09:44:02 -0800

    Threading added to word.py

--- word.py
+++ word.py
@@ -1,16 +1,35 @@
-from DBcm import useDatabase
-from flask import Flask, render_template, request, escape
+from DBcm import useDatabase, ConnectionError, CredentialsError
+from flask import Flask, render_template, request, escape, session
+from flask import copy_current_request_context
+from checker import check_logged_in
 from funtions import search4letters
+from threading import Thread
+
 app = Flask(__name__)
 app.config['dbconfig'] = {'host':'127.0.0.1', 'user':'root', 'database':'wordlogdb',}
 
+
 @app.route('/search', methods=['POST'])
-def do_search() -> str:
+def do_search() -> 'html':
+    @copy_current_request_context
+    def log_request(req: 'flask_request', res: str) -> None:
+        sleep(15) # This makes log_request to delay for 15 seconds
+        '''Log details of the web request and the results '''
+        with useDatabase(app.config['dbconfig']) as cursor:
+            _SQL = '''insert into log (phrase, letters, ip, browser_string, results) values (%s,%s,%s,%s,%s) '''
+            cursor.execute(_SQL,(req.form['phrase'], req.form['letters'], req.remote_addr, req.user_agent.browser, res, ))
+
+
     phrase= request.form['phrase']
     letters= request.form['letters'] 
     title= 'Here are your results: '
     results= str(search4letters(phrase, letters))
-    log_request(request, results)
+    try:
+        t = Thread(target=log_reques, args=(request, results))
+        t.start()
+        
+    except Exception as err:
+        print('****Logging failed with this error : ',str(err))
     return render_template('result.html',
                             the_phrase= phrase,
 			    the_letters= letters,
@@ -22,26 +41,29 @@ def entry_page() -> 'html':
     return render_template('entry.html',
 			    the_title='Welcome to this page')
 
-def log_request(req: 'flask_request', res: str) -> None:
-    '''Log details of the web request and the results '''
-    with useDatabase(app.config['dbconfig']) as cursor:
-        _SQL = '''insert into log (phrase, letters, ip, browser_string, results) values (%s,%s,%s,%s,%s) '''
-        cursor.execute(_SQL,(req.form['phrase'], req.form['letters'], req.remote_addr, req.user_agent.browser, res, ))
-
 
 @app.route('/viewlog')
+@check_logged_in
 def view_the_log() -> 'html':
-    contents= []
-    with useDatabase(app.config['dbconfig']) as cursor:
-        _SQL = '''select phrase, letters, ip, browser_string, results from log '''
-        cursor.execute(_SQL)
-        contents = cursor.fetchall()   
-    titles = ('phrase','letters', 'Remote_addr', 'User_agent', 'Results')
-    return render_template('viewlog.html',
-                            the_title='View Log',
-                            the_row_titles= titles,
-                            the_data= contents,)
-     
+    try:
+        with useDatabase(app.config['dbconfig']) as cursor:
+            _SQL = '''select phrase, letters, ip, browser_string, results from log '''
+            cursor.execute(_SQL)
+            contents = cursor.fetchall()   
+        titles = ('phrase','letters', 'Remote_addr', 'User_agent', 'Results')
+        return render_template('viewlog.html',
+                                the_title='View Log',
+                                the_row_titles= titles,
+                                the_data= contents,)
+    except ConnectionError as err:
+        print('Is your database switched on? Error:', str(err))
+    except CredentialsError as err:
+        print('user or password is incorrect : ', str(err))
+    except SQLError as err:
+        print('Is your query correct ? : ',str(err))
+    except Exception as err:
+        print('Something went wrong:', str(err))
+    return 'Error'
     '''with open('wordsearch.log') as log:
         for line in log: 
             contents.append([])
@@ -53,6 +75,18 @@ def view_the_log() -> 'html':
                             the_row_titles= titles,
                             the_data= contents,)'''
   
+@app.route('/login')
+def do_login()-> str:
+    session['logged_in'] = True
+    return 'You are now logged in'
+
+@app.route('/logout')
+def do_logout()-> str:
+    session.pop('logged_in')
+    return 'You are now logged out'
+
+app.secret_key = 'YouWillNeverGuessMysecretKey'
+
     
 if __name__ == '__main__':
     app.run(debug=True)
